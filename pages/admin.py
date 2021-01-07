1f0251fd0354f37e65ffede0a942e945d49d0ad1
AlexMuia31 <alexmuia31@gmail.com>
2021-01-07 10:36:59 +0300

    models and admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import home
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.site_header ='Real Estate' #changes the header of the admin page
+admin.site.unregister(Group) #removes group from admin
+admin.site.register(home)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class home(models.Model):
+    picture= models.ImageField()
+    name= models.CharField(max_length=100)
+    description= models.TextField()
+    address= models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+    
